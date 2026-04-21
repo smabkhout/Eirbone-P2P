d6eccb0a609b4bdf383f15be8f0e883eb3f698fa
DAMMOU Adam <adammou@bordeaux-inp.fr>
2026-04-21 16:58:22 +0200

    travail en mieux

--- java/FileMetadata.java
+++ java/FileMetadata.java
@@ -1,38 +1,34 @@
 import java.io.File;
 
-enum FileState {
-    SEED,
-    LEECH,
-    NONE
-}
+enum FileState { SEED, LEECH, NONE }
 
 public class FileMetadata {
-    private final String MD5hash;
-    private final long size;
-    private File localPath;
-    private String bufferMap;
-    private FileState state;
-    
-    public FileMetadata(String MD5hash, String path, long size, FileState state) {
-        this.MD5hash = MD5hash;
-        this.localPath = new File(path);
-        this.size = size;
-        this.bufferMap = "";
-        this.state = state;
-    }
+  private final String MD5hash;
+  private final long size;
+  private File localPath;
+  private String bufferMap;
+  private FileState state;
 
-    public FileMetadata(String MD5hash, String path, long size) {
-        this(MD5hash, path, size, FileState.NONE);
-    }
+  public FileMetadata(String MD5hash, String path, long size, FileState state) {
+    this.MD5hash = MD5hash;
+    this.localPath = new File(path);
+    this.size = size;
+    this.bufferMap = "";
+    this.state = state;
+  }
 
-    public String getFileName() { return localPath.getName(); }
-    public String getHash() { return MD5hash; }
-    public long getSize() { return size; }
-    public File getlocalPath() { return localPath; }
-    public void setlocalPath(File file) { this.localPath = file; }
-    public void setPath(String newPath) { this.localPath = new File(newPath); }
-    public String getBufferMap() { return bufferMap; }
-    public void setBufferMap(String bufferMap) { this.bufferMap = bufferMap; }
-    public FileState getState() { return state; }
-    public void setState(FileState state) { this.state = state; }
+  public FileMetadata(String MD5hash, String path, long size) {
+    this(MD5hash, path, size, FileState.NONE);
+  }
+
+  public String getFileName() { return localPath.getName(); }
+  public String getHash() { return MD5hash; }
+  public long getSize() { return size; }
+  public File getlocalPath() { return localPath; }
+  public void setlocalPath(File file) { this.localPath = file; }
+  public void setPath(String newPath) { this.localPath = new File(newPath); }
+  public String getBufferMap() { return bufferMap; }
+  public void setBufferMap(String bufferMap) { this.bufferMap = bufferMap; }
+  public FileState getState() { return state; }
+  public void setState(FileState state) { this.state = state; }
 }
